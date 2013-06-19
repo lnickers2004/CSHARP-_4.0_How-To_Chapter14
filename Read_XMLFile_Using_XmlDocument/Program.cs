28f2b03c7be6050ef2759c9dec680a8033c0db01
lnickers2004 <lnickers@gmail.com>
2013-06-19 18:09:10 -0500

    Added Read an Xmlfile Using XmlDocument

---
+++ Read_XMLFile_Using_XmlDocument/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Read_XMLFile_Using_XmlDocument
+{
+	class Program
+	{
+		const string sourceXml =
+	"<Book PublishYear=\"2009\">" +
+	"<Title>Programming, art or engineering?</Title>" +
+	"<Author>LeTigre Woods</Author>" +
+	"</Book>";
+
+		static void Main( string[] args )
+		{
+			XmlDocument doc = new XmlDocument();
+
+			doc.LoadXml(sourceXml);
+
+			Console.WriteLine("Publish year: {0}", doc.GetElementsByTagName("Book")[0].Attributes["PublishYear"].Value );
+
+			Console.WriteLine("Author: {0}", doc.GetElementsByTagName("Author")[0].InnerText);
+		}
+	}
+}
