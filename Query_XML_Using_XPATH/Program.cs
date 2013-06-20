8e52b0280e0a72a49be3ed586a484918f6004e80
lnickers2004 <lnickers@gmail.com>
2013-06-19 21:40:07 -0500

    Checkin of Query XML Using XPATH example

---
+++ Query_XML_Using_XPATH/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Query_XML_Using_XPATH
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			XPathDocument doc = new XPathDocument("LesMis.xml");
+
+			XPathNavigator navigator = doc.CreateNavigator();
+			XPathNodeIterator iter = navigator.Select("/Book/Chapters/Chapter");
+
+			while (iter.MoveNext())
+			{
+				Console.WriteLine("Chapter: {0}", iter.Current.Value);
+			}
+
+			Console.WriteLine( "Found {0} Chapters", navigator.Evaluate(("count(/Book/Chapters/Chapter)")) );
+
+
+			Console.WriteLine( "Press <Enter> to exit..." );
+			Console.ReadLine();
+		}
+
+
+
+	}
+}
