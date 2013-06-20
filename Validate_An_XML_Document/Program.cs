bdd040dc25c8667e5317922ab6880aa65a498c5a
lnickers2004 <lnickers@gmail.com>
2013-06-19 19:42:05 -0500

    Added Validate an XML Document Project

---
+++ Validate_An_XML_Document/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Validate_An_XML_Document
+{
+	class Program
+	{
+		const string sourceXml =
+		"<Book PublishYear=\"2009\">" +
+		"<Title>Programming, art or engineering?</Title>" +
+		"<Author>LeTigre Woods</Author>" +
+		"</Book>";
+
+		static void Main( string[] args )
+		{
+			XmlSchemaSet schemaSet = new XmlSchemaSet();
+
+			schemaSet.Add(null, "XmlBookSchema.xsd");
+
+			XmlReaderSettings settings = new XmlReaderSettings();
+			settings.ValidationType= ValidationType.Schema;
+			settings.Schemas = schemaSet;
+			settings.ValidationEventHandler += settings_ValidationEventHandler;
+
+			using(StringReader reader = new StringReader(sourceXml))
+			using (XmlReader xmlReader = XmlTextReader.Create(reader,settings))
+			{
+				while (xmlReader.Read()) ;
+			}
+		}
+
+		static void settings_ValidationEventHandler( object sender, ValidationEventArgs e )
+		{
+			//throw new NotImplementedException();
+			Console.WriteLine("Validation Failed: " + e.Message);
+		}
+	}
+}
