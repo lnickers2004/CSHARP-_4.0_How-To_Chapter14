5fc7c9aaaac1151d4033469b6528aeefb490fc93
lnickers2004 <lnickers@gmail.com>
2013-06-19 22:00:54 -0500

    Added Project to Transform XML to HTML and added the default .xslt file

---
+++ Transform_XML_To_Html/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transform_XML_To_Html
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+		}
+	}
+}
